98e7202897c8cefe5e5e32d8fdaf76c62cb6bad4
Joel Scott <scotjoe@mysdhc.net>
2020-01-27 14:53:11 -0500

    using for loop to update arrival times within firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,69 +1,98 @@
 $(document).ready(function() {
-
+    //firebaseConfig
     const firebaseConfig = {
         
     };
 
-    // Initialize Firebase
+    //global variables
+    let trains;
+    let database;
+    let newTrainTR;
+    let tdName;
+    let tdDestination;
+    let tdFrequency;
+    let tdNextTrain;
+    let trainName;
+    let trainDestination;
+    let firstTrain;
+    let trainFrequency;
+    let schedule = {};
+    
+
+    //Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     
-    let database = firebase.database();
+    database = firebase.database();
 
     function renderTrainSchedule(trains) {
         $('#train-schedule').empty();
         for (var i = 0; i < trains.length; i++) {
-            let newTrain = $('<tr>');
-            $('#train-schedule').append(newTrain);
-            let tdName = $('<td>' + trains[i].name + '</th>');
-            let tdDestination = $('<td>' + trains[i].destination + '</td>') 
-            let tdFrequency = $('<td>' + trains[i].frequency + '</td>') 
-            let tdNextTrain = $('<td>' + trains[i].first + '</td>')
-            $(newTrain).append(tdName, tdDestination, tdFrequency, tdNextTrain);
+            newTrainTR = $('<tr>');
+            $('#train-schedule').append(newTrainTR);
+            tdName = $('<td>' + trains[i].name + '</th>');
+            tdDestination = $('<td>' + trains[i].destination + '</td>'); 
+            tdFrequency = $('<td>' + trains[i].frequency + '</td>') ;
+            tdNextTrain = $('<td>' + trains[i].time + '</td>');
+            tdMinutesAway = $('<td>' + trains[i].minutes + '</td>');
+            $(newTrainTR).append(tdName, tdDestination, tdFrequency, tdNextTrain, tdMinutesAway);
         }
-        
     }
 
-    function addZero(i) {
-        if (i < 10) {
-            i = "0" + i;
+    function initializeSchedule() {
+        event.preventDefault();
+        trainName = $('#train-name').val().trim();
+        trainDestination = $('#destination').val().trim();
+        firstTrain = $('#first-train-time').val().trim();
+        trainFrequency = $('#frequency').val().trim();
+        var trainTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+        var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+        var timeRemainder = diffTime % trainFrequency;
+        var minutesLeft = trainFrequency - timeRemainder;
+        var trainArrival = moment().add(minutesLeft, "minutes").format("hh:mm");
+        schedule = {name: trainName, destination: trainDestination, first: firstTrain, frequency: trainFrequency, minutes: minutesLeft, time: trainArrival};
+    }
+
+    function refreshTimes(snapshot) {
+        trains = snapshot.val().trains;
+        for (var i = 0; i < trains.length; i++) {
+            trainName = snapshot.val().trains[i].name;
+            trainDestination = snapshot.val().trains[i].destination;
+            firstTrain = snapshot.val().trains[i].first;
+            trainFrequency = snapshot.val().trains[i].frequency;
+            //use database items to find arrival time and minutes away
+            //initial train time, subtracted one year off so it comes before current time
+            var trainTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+            //subtract current time from first train time and return result in minutes
+            var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+            //use modulus to get the remainder, which is the number of minutes left 
+            var timeRemainder = diffTime % trainFrequency;
+            //time from the frequency
+            var newminutesLeft = trainFrequency - timeRemainder;
+            //add minutes left to current time and return result in minutes
+            var newtrainArrival = moment().add(newminutesLeft, "minutes").format("hh:mm");
+            trains[i] = {name: trainName, destination: trainDestination, first: firstTrain, frequency: trainFrequency, minutes: newminutesLeft, time: newtrainArrival}; 
         }
-        return i;
-      }
-      
-    function time() {
-        var date = new Date();
-        var hour = addZero(date.getHours());
-        var minutes = addZero(date.getMinutes());
-        var currentTime = hour + ":" + minutes;
-        console.log(currentTime);
     }
     
 
     $('#add-train').on('click', function(event) {
-        let schedule = {};
-        event.preventDefault();
-        let trainName = $('#train-name').val().trim();
-        let trainDestination = $('#destination').val().trim();
-        let firstTrain = $('#first-train-time').val().trim();
-        let trainFrequency = parseInt($('#frequency').val().trim());
-        schedule = {name: trainName, destination: trainDestination, first: firstTrain, frequency: trainFrequency};
+        initializeSchedule();
         $('#train-name').val('');
         $('#destination').val('');
         $('#first-train-time').val('');
         $('#frequency').val('');
-        console.log(schedule);
         trains.push(schedule);
-        renderTrainSchedule(trains);
         database.ref().set({
             trains: trains
         })
     })
 
     database.ref().on('value', function(snapshot) {
-        if (snapshot.child("trains").exists()) {
+        if (snapshot.child('trains').exists()) {
             console.log(snapshot.val());
             console.log(snapshot.val().trains);
             trains = snapshot.val().trains;
+            refreshTimes(snapshot);
             renderTrainSchedule(trains);
         }
         
@@ -72,10 +101,7 @@ $(document).ready(function() {
         console.log("The read failed: " + errorObject.code);
     });
 
-    let trains;
     if (!Array.isArray(trains)) {
         trains = [];
     }
-
-    time();
 });
